13bb166160ecc67b26d0076ddaecce372a5820de
dhankarsimran <83290371+dhankarsimran@users.noreply.github.com>
2022-07-27 12:10:35 +0530

    third commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,44 @@ const navbar = () => {
       <div className="n-right">
       <input type="checkbox" id="checkbox_toggle" />
         <label for="checkbox_toggle" className="hamburger">&#9776;</label>
+             {/* HamList */}
+         <div className="ham-list">
+         <ul style={{ listStyleType: "none" }}>
+          <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="experience" spy={true} smooth={true}>
+                Education
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Protfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Certification
+              </Link>
+            </li>
+            </ul>
+         </div>
+
+
+       
         <div className="n-list">
 
         <ul style={{ listStyleType: "none" }}>
